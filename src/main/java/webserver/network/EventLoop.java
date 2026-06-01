3eac5d9e27ee7b057e0434e34b16a5184bc7c641
devex <loan.mata@ynov.com>
2026-06-01 17:50:09 +0200

    feat(network): Implementing a pseudo event-loop not calling any action for now only printing log

--- src/main/java/webserver/network/EventLoop.java
+++ src/main/java/webserver/network/EventLoop.java
@@ -1,5 +1,45 @@
 package webserver.network;
 
+import java.nio.channels.SelectionKey;
+
 public class EventLoop {
+ 
+    private boolean running = false;
+    private SelectorManager selectorManager = SelectorManager.getInstance();
+    private int errorCount = 0;
+
+    public void start() {
+        running = true;
+        while (running) {
+            try {
+                selectorManager.select();
+                
+                for (SelectionKey key : selectorManager.selectedKeys()) {
+                    if (key.isAcceptable()) {
+                        System.out.println("Accepting new connection...");
+                    } else if (key.isReadable()) {
+                        System.out.println("Reading data from client..." + key.attachment());
+                    } else if (key.isWritable()) {
+                        System.out.println("Writing data to client..." + key.attachment());
+                    }
+                }
+
+            } catch (Exception e) {
+                System.err.println("Error in event loop: " + e.getMessage());
+                System.err.println("Stack trace:");
+                e.printStackTrace();
+                errorCount++;
+                if(errorCount >= 5) {
+                    System.err.println("Too many errors in event loop, stopping...");
+                    stop();
+                }
+                System.err.println("Continuing event loop...");
+            }
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
     
 }
