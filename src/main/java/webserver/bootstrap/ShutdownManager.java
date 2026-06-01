ba5b8a983361d328503bed43726531148d656e0d
devex <loan.mata@ynov.com>
2026-06-01 22:28:19 +0200

    feat(bootstrap): Implement ShutdownManager as centralized shutdown hook (replaces duplicate hooks)

--- src/main/java/webserver/bootstrap/ShutdownManager.java
+++ src/main/java/webserver/bootstrap/ShutdownManager.java
@@ -1,5 +1,35 @@
 package webserver.bootstrap;
 
+import webserver.network.EventLoop;
+import webserver.network.SelectorManager;
+
 public class ShutdownManager {
-    
+
+    private EventLoop eventLoop;
+    private static ShutdownManager instance;
+
+    private ShutdownManager() {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
+    }
+
+
+    public static synchronized ShutdownManager getInstance() {
+        if (instance == null) {
+            instance = new ShutdownManager();
+        }
+        return instance;
+    }
+
+    public void register(EventLoop eventLoop) {
+        this.eventLoop = eventLoop;
+    }
+
+    public void shutdown() {
+        System.out.println("Shutting down all servers...");
+        if (eventLoop != null) {
+            eventLoop.stop();
+        }
+        SelectorManager.getInstance().closeChannels();
+        System.out.println("Server stopped.");
+    }
 }
