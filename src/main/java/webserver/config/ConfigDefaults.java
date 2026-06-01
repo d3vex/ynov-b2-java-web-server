0b071c57f5c182987025419560e5805c4efae3d6
devex <loan.mata@ynov.com>
2026-06-01 22:27:57 +0200

    feat(config): Add config defaults constants (timeout, body limit, server root, default file, directory listing)

--- src/main/java/webserver/config/ConfigDefaults.java
+++ src/main/java/webserver/config/ConfigDefaults.java
@@ -1,5 +1,13 @@
 package webserver.config;
 
 public class ConfigDefaults {
-    
+
+    public static final long TIMEOUT_MS = 30000;
+    public static final long CLIENT_BODY_LIMIT = 1024 * 1024;
+    public static final String DEFAULT_SERVER_ROOT = "www";
+    public static final String DEFAULT_FILE = "index.html";
+    public static final boolean DIRECTORY_LISTING = false;
+
+    private ConfigDefaults() {
+    }
 }
