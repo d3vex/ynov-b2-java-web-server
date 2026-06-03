e55832329c75f3a5fb64a3b076a3addd70aedacf
devex <loan.mata@ynov.com>
2026-06-03 19:25:03 +0200

    refactor(errors): Implement ServerExceptionMapper to map exceptions (FileNotFoundException, SecurityException, etc.) to HttpStatus

--- src/main/java/webserver/errors/ServerExceptionMapper.java
+++ src/main/java/webserver/errors/ServerExceptionMapper.java
@@ -1,5 +1,29 @@
 package webserver.errors;
 
+import java.io.FileNotFoundException;
+import java.nio.channels.ClosedChannelException;
+
+import webserver.http.HttpStatus;
+
 public class ServerExceptionMapper {
-    
+
+    public HttpStatus toStatus(Exception e) {
+        if (e instanceof FileNotFoundException) {
+            return HttpStatus.NOT_FOUND;
+        }
+        if (e instanceof SecurityException) {
+            return HttpStatus.FORBIDDEN;
+        }
+        if (e instanceof IllegalArgumentException) {
+            return HttpStatus.BAD_REQUEST;
+        }
+        if (e instanceof ClosedChannelException) {
+            return HttpStatus.SERVICE_UNAVAILABLE;
+        }
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    public HttpStatus toStatus(HttpStatus status) {
+        return status;
+    }
 }
