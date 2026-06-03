fc9c888935e2e3e9e60aa85d07b84700e69d46d5
devex <loan.mata@ynov.com>
2026-06-03 19:25:02 +0200

    refactor(errors): Implement DefaultErrorPages with render(HttpStatus) and render(HttpStatus, String) for inline HTML generation

--- src/main/java/webserver/errors/DefaultErrorPages.java
+++ src/main/java/webserver/errors/DefaultErrorPages.java
@@ -1,5 +1,24 @@
 package webserver.errors;
 
+import webserver.http.HttpStatus;
+
 public class DefaultErrorPages {
-    
+
+    public String render(HttpStatus status) {
+        return render(status, status.getReasonPhrase());
+    }
+
+    public String render(HttpStatus status, String message) {
+        int code = status.getCode();
+        return """
+                <!DOCTYPE html>
+                <html>
+                <head><title>%d %s</title></head>
+                <body>
+                <h1>%d %s</h1>
+                <hr>
+                </body>
+                </html>
+                """.formatted(code, message, code, message);
+    }
 }
